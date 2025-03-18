dccf95045b0fc44d056c0eed5f3bf1dae03cf762
unknown <sebasguesa@hotmail.com>
2025-03-18 17:19:41 -0500

    Lenguaje Go con variables

---
+++ 2_Variables.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nombrePersona string = "Sebas"
+	var apellidoPersona = "Guevara"
+	edad := 25
+	var anhoactual int16 = 2024
+	var anhoreducido int8 = 24
+
+	fmt.Println("Hola mundo, soy ", nombrePersona)
+	fmt.Println("Mi apellido es ", apellidoPersona)
+
+	fmt.Println("la edad es ", edad)
+	fmt.Println("Año actual es ", anhoactual)
+	fmt.Println("Año abreviado es ", anhoreducido)
+
+}
